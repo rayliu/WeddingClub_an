8a4d9cdf362fd5aafc2935e51cb811acdbc22c22
Ray Liu <liuyubie@gmail.com>
2017-12-11 14:43:04 +0800

    精选婚礼: 修改UI样式

--- app/src/main/java/com/eeda123/wedding/bestCase/CaseDetailActivity.java
+++ app/src/main/java/com/eeda123/wedding/bestCase/CaseDetailActivity.java
@@ -1,15 +1,58 @@
 package com.eeda123.wedding.bestCase;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.eeda123.wedding.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class CaseDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.action_bar_title)
+    TextView action_bar_title;
+    @BindView(R.id.cityChange)
+    LinearLayout cityChange;
+    @BindView(R.id.img_back_arrow)
+    ImageView img_back_arrow;
+    @BindView(R.id.back_arrow)
+    LinearLayout back_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_detail);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //返回箭头（默认不显示）
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            // 使左上角图标(系统)是否显示
+            actionBar.setDisplayShowHomeEnabled(false);
+            // 显示标题
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(true);
+
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); //Enable自定义的View
+            actionBar.setCustomView(R.layout.header_bar);//设置自定义的布局：header_bar
+        }
+        ButterKnife.bind(this);
+
+        action_bar_title.setText("精选婚礼");
+        cityChange.setVisibility(View.GONE);
+        img_back_arrow.setVisibility(View.VISIBLE);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.back_arrow})
+    public void onBack_arrowClick(View view) {
+        finish();
     }
 }
