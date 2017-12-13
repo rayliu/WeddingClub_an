976a5128d22656e1209d6233308406afe1f351ad
kevin <kevin.dou@eeda123.com>
2017-12-13 18:09:31 +0800

    视频数据带出

--- app/src/main/java/com/eeda123/wedding/shop/VideoActivity.java
+++ app/src/main/java/com/eeda123/wedding/shop/VideoActivity.java
@@ -1,8 +1,13 @@
 package com.eeda123.wedding.shop;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -11,13 +16,37 @@ import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.eeda123.wedding.HomeFragment;
+import com.eeda123.wedding.MainActivity;
 import com.eeda123.wedding.R;
+import com.eeda123.wedding.consult.ConsultActivity;
+import com.eeda123.wedding.login.LoginActivity;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.eeda123.wedding.MainActivity.HOST_URL;
 import static com.eeda123.wedding.R.id.webView;
 
 
@@ -33,6 +62,14 @@ public class VideoActivity extends AppCompatActivity {
     ImageView img_back_arrow;
     @BindView(R.id.back_arrow)
     LinearLayout back_arrow;
+
+    @BindView(R.id.shop_name) TextView shopName;
+    @BindView(R.id.shop_logo) ImageView shopLogo;
+    @BindView(R.id.category_name) TextView categoryName;
+    @BindView(R.id.title) TextView title;
+
+    private Long case_id;
+    private Long shop_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +95,107 @@ public class VideoActivity extends AppCompatActivity {
         cityChange.setVisibility(View.GONE);
         img_back_arrow.setVisibility(View.VISIBLE);
 
-        webview = (WebView) findViewById(webView);
-        webview.setWebViewClient(new WebViewClient());
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webview.getSettings().setPluginState(WebSettings.PluginState.ON);
-        webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
-        webview.setWebChromeClient(new WebChromeClient());
-        webview.loadUrl("http://player.youku.com/embed/XMzIyMzc2MDc0MA==");
+        Bundle bundle = new Bundle();
+        bundle = this.getIntent().getExtras();
+        case_id = bundle.getLong("case_id");
+
+        getData();
+    }
+
+
+    private void getData() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request request = original.newBuilder()
+                        .header("case_id", case_id.toString())
+                        .method(original.method(), original.body())
+                        .build();
+
+                return chain.proceed(request);
+            }
+        });
+
+        OkHttpClient client = httpClient.build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(HOST_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        HomeFragment.EedaService service = retrofit.create(HomeFragment.EedaService.class);
+
+
+        Call<HashMap<String, Object>> call = service.list("bestCase","video_case");
+
+        call.enqueue(eedaCallback());
+    }
+
+
+    @NonNull
+    private Callback<HashMap<String,Object>> eedaCallback() {
+        return new Callback<HashMap<String,Object>>() {
+            @Override
+            public void onResponse(Call<HashMap<String,Object>> call, Response<HashMap<String,Object>> response) {
+                // The network call was a success and we got a response
+                HashMap<String,Object> json = response.body();
+                ArrayList<Map> shopList =  (ArrayList<Map>)json.get("DATA");
+
+                String c_shop_name = null;
+                String c_category_name = null;
+                String c_title = null;
+                String c_video_url = null;
+                String c_shop_logo = null;
+
+                if(shopList.get(0).get("C_NAME") != null){
+                    c_shop_name = shopList.get(0).get("C_NAME").toString();
+                }
+                if(shopList.get(0).get("CREATOR") != null){
+                    shop_id = ((Double)shopList.get(0).get("CREATOR")).longValue();
+                }
+                if(shopList.get(0).get("CATEGORY_NAME") != null){
+                    c_category_name = shopList.get(0).get("CATEGORY_NAME").toString();
+                }
+                if(shopList.get(0).get("TITLE") != null){
+                    c_title = shopList.get(0).get("TITLE").toString();
+                }
+                if(shopList.get(0).get("VIDEO_URL") != null){
+                    c_video_url = shopList.get(0).get("VIDEO_URL").toString();
+                }
+                if(shopList.get(0).get("LOGO") != null){
+                    c_shop_logo  = shopList.get(0).get("LOGO").toString();
+                }
+
+                Picasso.with(getBaseContext()).load(MainActivity.HOST_URL+"upload/"+c_shop_logo)
+                        .into(shopLogo);
+                shopName.setText(c_shop_name);
+                categoryName.setText("类别："+c_category_name);
+                title.setText(c_title);
+
+                webview = (WebView) findViewById(webView);
+                webview.setWebViewClient(new WebViewClient());
+                webview.getSettings().setJavaScriptEnabled(true);
+                webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                webview.getSettings().setPluginState(WebSettings.PluginState.ON);
+                webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+                webview.setWebChromeClient(new WebChromeClient());
+                webview.loadUrl(c_video_url);
+            }
+
+            @Override
+            public void onFailure(Call<HashMap<String,Object>> call, Throwable t) {
+                // the network call was a failure
+                Toast.makeText(getBaseContext(), "网络连接失败", Toast.LENGTH_LONG).show();
+            }
+        };
     }
 
     @OnClick({R.id.back_arrow})
@@ -80,5 +210,35 @@ public class VideoActivity extends AppCompatActivity {
         super.onDestroy();
         //一定要销毁，否则无法停止播放
         webview.destroy();
+    }
+
+
+    @OnClick({R.id.consult})
+    public void onConsultClick(View view) {
+        //同样，在读取SharedPreferences数据前要实例化出一个SharedPreferences对象
+        SharedPreferences sharedPreferences = getSharedPreferences("login_file",
+                Activity.MODE_PRIVATE);
+        // 使用getString方法获得value，注意第2个参数是value的默认值
+        String login_id = sharedPreferences.getString("login_id", "");
+        String mobile = sharedPreferences.getString("mobile", "");
+        String user_name = sharedPreferences.getString("user_name", "");
+        String wedding_date = sharedPreferences.getString("wedding_date", "");
+        if (TextUtils.isEmpty(login_id)) {
+            Toast.makeText(this, "您未登录，请前往登录", Toast.LENGTH_LONG).show();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            };
+            (new Timer()).schedule(task, 2000);
+        } else {
+            Intent intent = new Intent(this, ConsultActivity.class);
+            intent.putExtra("shop_id", shop_id);
+            intent.putExtra("shop_name", shopName.getText());
+            intent.putExtra("category", categoryName.getText());
+            startActivity(intent);
+        }
     }
 }
