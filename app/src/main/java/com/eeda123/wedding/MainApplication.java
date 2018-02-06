350e1e909fd4837ed4cbadc5052f9f8ac53fce9d
Ray Liu <liuyubie@gmail.com>
2018-02-06 15:18:23 +0800

    APP加上接收推送消息的类

---
+++ app/src/main/java/com/eeda123/wedding/MainApplication.java
@@ -0,0 +1,52 @@
+package com.eeda123.wedding;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.alibaba.sdk.android.push.CloudPushService;
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+import com.alibaba.sdk.android.push.register.GcmRegister;
+import com.alibaba.sdk.android.push.register.HuaWeiRegister;
+import com.alibaba.sdk.android.push.register.MiPushRegister;
+
+/**
+ * Created by a13570610691 on 2018/2/5.
+ */
+
+public class MainApplication extends Application {
+    private static final String TAG = "Init";
+    private static MainActivity mainActivity = null;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initCloudChannel(this);
+    }
+    /**
+     * 初始化云推送通道
+     * @param applicationContext
+     */
+    private void initCloudChannel(final Context applicationContext) {
+        PushServiceFactory.init(applicationContext);
+        final CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.register(applicationContext, new CommonCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Log.i(TAG, "init cloudchannel success");
+
+            }
+
+            @Override
+            public void onFailed(String errorCode, String errorMessage) {
+                Log.e(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
+            }
+        });
+
+//        MiPushRegister.register(applicationContext, "XIAOMI_ID", "XIAOMI_KEY"); // 初始化小米辅助推送
+//        HuaWeiRegister.register(applicationContext); // 接入华为辅助推送
+//        GcmRegister.register(applicationContext, "send_id", "application_id"); // 接入FCM/GCM初始化推送
+    }
+
+
+}
